53baf0f496c7eb60c10a441269ecb3b365a02abb
SergeyGolenko <1989sergiotay@gmail.com>
2019-09-04 15:27:18 +0300

    Initial Commit

---
+++ UIWebView/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  UIWebView
+//
+//  Created by Sergey Golenko on 04/09/2019.
+//  Copyright © 2019 Sergey Golenko. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
